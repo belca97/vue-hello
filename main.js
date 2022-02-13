6ab5cde5296cc99947ac30605b8f8939e2376c68
matteo belcari <matteobelcari977@gmail.com>
2022-02-13 16:44:43 +0100

    esercizio finito

---
+++ main.js
@@ -0,0 +1,9 @@
+const root = new Vue(
+    {
+        el: '#root',
+        data : {
+            messaggio: 'Hello world',
+            immagine : 'https://www.laramind.com/blog/wp-content/uploads/2019/09/Untitled-design-2019-09-03T162841.927.png'
+        }
+    }
+)
